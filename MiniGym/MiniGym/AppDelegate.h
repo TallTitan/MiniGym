58ce187991e9175a7dd427e39f54903ba12beabe
seaschi <seaschi@seans-mac-pro.guest.corp.microsoft.com>
2016-08-04 10:12:56 -0700

    new files added

---
+++ MiniGym/MiniGym/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MiniGym
+//
+//  Created by seaschi on 8/4/16.
+//  Copyright © 2016 seaschi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MiniGym/MiniGym/CollectionViewController.h
@@ -0,0 +1,13 @@
+//
+//  CollectionViewController.h
+//  MiniGym
+//
+//  Created by seaschi on 8/4/16.
+//  Copyright © 2016 seaschi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CollectionViewController : UICollectionViewController
+
+@end
